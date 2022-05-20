901da06b01a255275b4dfa10c2a1b017d2f664f8
ester kim <kimester1996@gmail.com>
2022-05-20 15:38:10 -0800

    still

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,23 +1,29 @@
-console.log("news linked")
+console.log("news linked");
 
-document.querySelector("#newBlog").addEventListener("submit",e=>{
-    e.preventDefault();
-    const blogObj = {
-        title:document.querySelector("#title").value,
-        post:document.querySelector("#post").value,
+ const title = document.querySelector("#title");
+  const body = document.querySelector("#body");
+
+document.querySelector("#newBlog").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const blogObj = {
+    title: title.value,
+    body: body.value,
+  };
+ 
+
+  console.log(blogObj);
+  fetch("/api/blogs/", {
+    method: "POST",
+    body: JSON.stringify(blogObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.ok) {
+      location.reload();
+    } else {
+      alert("trumpet sound");
     }
-    console.log(blogObj)
-    fetch("/api/blogs/",{
-        method:"POST",
-        body:JSON.stringify(blogObj),
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(res=>{
-        if(res.ok){
-            location.reload()
-        } else {
-            alert("trumpet sound")
-        }
-    })
-})
+  });
+});
